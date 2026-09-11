fe0e305ca76001ca7df2b218e075ae5e71ff5da8
VieyraUlises <uliivieyra@gmail.com>
2026-09-11 15:59:55 -0300

    agregar catálogo inicial de canciones con versiones en canciones.py
    Se creó el archivo src/dominio/canciones.py con un catálogo de ejemplo
    de 17 canciones. Cada ítem incluye atributos: id, título, artista,
    álbum, año, versión, duración y género. 
    
    El catálogo refleja la relación recursiva mediante versiones (en vivo,
    remix, cover, etc.) y servirá como base para la primera entrega (E1),
    donde el menú principal podrá listar estos ítems.

---
+++ src/dominio/canciones.py
@@ -0,0 +1,20 @@
+catalogo = [
+  {"id": 1, "titulo": "Yegua", "artista": "Babasónicos", "album": "Anoche", "anio": 2005, "version": "Album Version", "duracion": "2:29", "genero": "Rock"},
+  {"id": 2, "titulo": "Yegua", "artista": "Babasónicos", "album": "Luces", "anio": 2006, "version": "En vivo", "duracion": "2:43", "genero": "Rock"},
+  {"id": 3, "titulo": "Don't Phunk with My Heart", "artista": "The Black Eyed Peas", "album": "Monkey Business", "anio": 2005, "version": "Album Version", "duracion": "3:59", "genero": "Hip hop / Pop"},
+  {"id": 4, "titulo": "Don't Phunk with My Heart (Chicago House Remix)", "artista": "The Black Eyed Peas", "album": "Single", "anio": 2005, "version": "Remix", "duracion": "3:46", "genero": "House"},
+  {"id": 5, "titulo": "Ghosts 'n' Stuff", "artista": "deadmau5", "album": "Single", "anio": 2008, "version": "Original Instrumental Version", "duracion": "6:10", "genero": "Electro house"},
+  {"id": 6, "titulo": "Ghosts 'n' Stuff", "artista": "deadmau5 feat. Rob Swire", "album": "For Lack of a Better Name", "anio": 2009, "version": "Album / Vocal Version", "duracion": "5:28", "genero": "Electro house"},
+  {"id": 7, "titulo": "Be Together", "artista": "Major Lazer feat. Wild Belle", "album": "Peace Is the Mission", "anio": 2015, "version": "Album Version", "duracion": "3:53", "genero": "EDM / Pop rock / R&B"},
+  {"id": 8, "titulo": "Be Together (L D R U Remix)", "artista": "Major Lazer feat. Wild Belle", "album": "Australazer", "anio": 2015, "version": "Remix", "duracion": "2:26", "genero": "Electronic"},
+  {"id": 9, "titulo": "Rosas", "artista": "La Oreja de Van Gogh", "album": "Lo Que Te Conté Mientras Te Hacías la Dormida", "anio": 2003, "version": "Album Version", "duracion": "3:56", "genero": "Pop"},
+  {"id": 10, "titulo": "Rosas", "artista": "La Oreja de Van Gogh", "album": "La Oreja de Van Gogh en Directo", "anio": 2004, "version": "En vivo", "duracion": "3:55", "genero": "Pop"},
+  {"id": 11, "titulo": "Algo Contigo", "artista": "Vicentico", "album": "Vicentico", "anio": 2002, "version": "Cover", "duracion": "3:38", "genero": "Pop rock"},
+  {"id": 12, "titulo": "Algo Contigo", "artista": "Chico Novarro", "album": "Algo Contigo", "anio": 1976, "version": "Versión original", "duracion": "3:23", "genero": "Bolero"},
+  {"id": 13, "titulo": "My My My", "artista": "Armand Van Helden", "album": "Nympho", "anio": 2005, "version": "Album Version", "duracion": "3:02", "genero": "Dance"},
+  {"id": 14, "titulo": "Dani California", "artista": "Red Hot Chili Peppers", "album": "Stadium Arcadium", "anio": 2006, "version": "Album Version", "duracion": "4:42", "genero": "Funk rock"},
+  {"id": 15, "titulo": "The Bakery", "artista": "Arctic Monkeys", "album": "Fluorescent Adolescent single", "anio": 2007, "version": "Studio", "duracion": "2:56", "genero": "Indie rock"},
+  {"id": 16, "titulo": "Deja Vu", "artista": "Gustavo Cerati", "album": "Fuerza Natural", "anio": 2009, "version": "Album Version", "duracion": "3:24", "genero": "Pop rock"},
+  {"id": 17, "titulo": "Deja Vu (Leandro Fresco Dance Mix)", "artista": "Gustavo Cerati", "album": "Single", "anio": 2009, "version": "Remix", "duracion": "6:01", "genero": "Electronic"}
+]
+  
